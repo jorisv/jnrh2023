c07e77b185ed62e43d0587083bda21bc877d5481
Joris Vaillant <joris.vaillant@gmail.com>
2023-10-04 15:36:05 +0200

    Improve Slider API

--- utils/slider.py
+++ utils/slider.py
@@ -7,24 +7,14 @@ from IPython.display import display
 
 
 class Slider:
-    def __init__(self):
+    def __init__(self, cb: typing.Callable[[np.ndarray], None]):
         self.q_list: typing.List[np.ndarray] = []
         self.slider = widgets.IntSlider(value=0, min=0, max=0)
+        self.slider.observe(lambda change: cb(self.q_list[change.new]), names="value")
 
     def display(self):
+        self.slider.max = max(len(self.q_list) - 1, 0)
         display(self.slider)
-
-    def reset(self):
-        self.q_list = []
 
     def append_q(self, q: np.ndarray):
         self.q_list.append(q)
-
-    def update(self):
-        self.slider.value = 0
-        self.slider.max = max(len(self.q_list) - 1, 0)
-
-    def set_interaction(self, cb: typing.Callable[[np.ndarray], None]):
-        widgets.interactive_output(
-            lambda slider: cb(self.q_list[slider]), {"slider": self.slider}
-        )
